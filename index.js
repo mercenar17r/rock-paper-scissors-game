28388a5639d8b0a3c133def387fea471b9e86bfb
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-12-31 02:33:26 +0200

    refactor computer move with function

---
+++ index.js
@@ -0,0 +1,17 @@
+
+computerMove='';
+
+function pickComputerMove(){
+    randomNumber=Math.random()
+  
+      if (randomNumber>=0 && randomNumber <1/3){
+      computerMove='rock';
+
+      }else if(randomNumber>=1/3 && randomNumber <2/3){
+      computerMove='paper';
+      }else if(randomNumber>=2/3 && randomNumber <1){
+      computerMove='scissors';
+      }
+}
+
+
